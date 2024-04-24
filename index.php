51146e615e4cab83091ca32953c9d0fd0715c720
PheareakIT <RoeurnPheareak71@gmail.com>
2024-04-24 23:59:31 +0700

    Updated

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Home Page</h1>
+                            <h1 class="m-0">Home</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
