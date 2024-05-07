d27af041b62d4e295e96e8fdbfbaac3d8157126c
sengeys <sengeys@gmail.com>
2024-05-07 10:24:40 +0700

    new update layout

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -1,98 +1,98 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-  <!-- Brand Logo -->
-  <a href="index.php" class="brand-link">
-      <img src="dist/img/RestaurantLogo.png" alt="Restaurant Logo" class="brand-image img-circle elevation-3"
-          style="opacity: .8">
-      <span class="brand-text font-weight-light">RESTAURANT</span>
-  </a>
+    <!-- Brand Logo -->
+    <a href="../index.php" class="brand-link">
+        <img src="../dist/img/RestaurantLogo.png" alt="Restaurant Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
+        <span class="brand-text font-weight-light">RESTAURANT</span>
+    </a>
 
-  <!-- Sidebar -->
-  <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="image">
-              <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-          </div>
-          <div class="info">
-              <a href="#" class="d-block">Stone Univers</a>
-          </div>
-      </div>
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Stone Univers</a>
+            </div>
+        </div>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-              data-accordion="false">
-              <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
-              <li class="nav-item">
-                  <a href="index.php" class="nav-link">
-                      <i class="nav-icon fas fa-tachometer-alt"></i>
-                      <p>
-                          Dashboard
-                      </p>
-                  </a>
-              </li>
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="../index.php" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
 
-              <li class="nav-item">
-                  <a href="pages/staff.php" class="nav-link">
-                      <i class="nav-icon fas fa-users-cog"></i>
-                      <p>
-                          Staff
-                      </p>
-                  </a>
-              </li>
+                <li class="nav-item">
+                    <a href="../pages/staff.php" class="nav-link">
+                        <i class="nav-icon fas fa-users-cog"></i>
+                        <p>
+                            Staff
+                        </p>
+                    </a>
+                </li>
 
-              <li class="nav-item">
-                  <a href="pages/customer.php" class="nav-link">
-                      <i class="nav-icon fas fa-users"></i>
-                      <p>
-                          Customer
-                      </p>
-                  </a>
-              </li>
+                <li class="nav-item">
+                    <a href="../pages/customer.php" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            Customer
+                        </p>
+                    </a>
+                </li>
 
 
-              <li class="nav-item">
-                  <a href="pages/table.php" class="nav-link">
-                      <i class="nav-icon fas fa-columns"></i>
-                      <p>
-                          Table
-                      </p>
-                  </a>
-              </li>
+                <li class="nav-item">
+                    <a href="../pages/table.php" class="nav-link">
+                        <i class="nav-icon fas fa-columns"></i>
+                        <p>
+                            Table
+                        </p>
+                    </a>
+                </li>
 
-              <li class="nav-item">
-                  <a href="#" class="nav-link">
-                      <i class="nav-icon far fa-envelope"></i>
-                      <p>
-                          Mailbox
-                          <i class="fas fa-angle-left right"></i>
-                      </p>
-                  </a>
-                  <ul class="nav nav-treeview">
-                      <li class="nav-item">
-                          <a href="pages/mailbox/mailbox.html" class="nav-link">
-                              <i class="far fa-circle nav-icon"></i>
-                              <p>Inbox</p>
-                          </a>
-                      </li>
-                      <li class="nav-item">
-                          <a href="pages/mailbox/compose.html" class="nav-link">
-                              <i class="far fa-circle nav-icon"></i>
-                              <p>Compose</p>
-                          </a>
-                      </li>
-                      <li class="nav-item">
-                          <a href="pages/mailbox/read-mail.html" class="nav-link">
-                              <i class="far fa-circle nav-icon"></i>
-                              <p>Read</p>
-                          </a>
-                      </li>
-                  </ul>
-              </li>
-          </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-  </div>
-  <!-- /.sidebar -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon far fa-envelope"></i>
+                        <p>
+                            Mailbox
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="../pages/mailbox/mailbox.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Inbox</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../pages/mailbox/compose.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Compose</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../pages/mailbox/read-mail.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Read</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
 </aside>
