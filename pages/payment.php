95fbc8efbaf425bf279439f584c82a5178b29d65
sengeys <sengeys@gmail.com>
2024-05-11 20:48:32 +0700

    Update order.php and payment

--- pages/payment.php
+++ pages/payment.php
@@ -153,13 +153,8 @@
 
                                         <label>Status</label>
                                         <select class="form-control select2" style="width: 100%;">
-                                            <option selected="selected">Alabama</option>
-                                            <option>Alaska</option>
-                                            <option>California</option>
-                                            <option>Delaware</option>
-                                            <option>Tennessee</option>
-                                            <option>Texas</option>
-                                            <option>Washington</option>
+                                            <option selected="selected">No Paid</option>
+                                            <option>Paid</option>
                                         </select>
                                     </div>
                                 </div>
