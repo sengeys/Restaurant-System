054fb7790f2dc471ec32e98998a90f6805bdc9c5
sengeys <sengeys@gmail.com>
2024-06-07 15:25:10 +0700

    update

--- pages/item.php
+++ pages/item.php
@@ -170,7 +170,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4 class="modal-title">Insert Item</h4>
+                        <h4 class="modal-title">Delete Item</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
