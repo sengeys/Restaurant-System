8a474caa0fe2615764a4987e4c658249f1587e3f
sengeys <sengeys@gmail.com>
2024-06-18 07:21:04 +0700

    update order

--- pages/payment.php
+++ pages/payment.php
@@ -29,7 +29,7 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
+                            <li class="breadcrumb-item"><a href="../pages/dashboard.php">Home</a></li>
                             <li class="breadcrumb-item active">Payment</li>
                         </ol>
                     </div><!-- /.col -->
